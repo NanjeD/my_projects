b19365e99892daff0625e3e8117b74f435b1f558
NanjeD <basaruwaka@gmail.com>
2023-12-15 08:34:32 +0100

    Testing git security

--- test.sh
+++ test.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 echo "This is my first project"
+echo " Testing git security 
+#installing git security 
+#   curl --silent  https://raw.githubusercontent.com/thoughtworks/talisman/master/global_install_scripts/install.bash > /tmp/install_talisman.bash && /bin/bash /tmp/install_talisman.bash
