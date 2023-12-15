62448073234ad0fd999b850cd68806883629ab4d
NanjeD <basaruwaka@gmail.com>
2023-12-15 08:54:28 +0100

    Modified fromm talisman

--- test.sh
+++ test.sh
@@ -3,3 +3,8 @@ echo "This is my first project"
 echo " Testing git security 
 #installing git security 
 #   curl --silent  https://raw.githubusercontent.com/thoughtworks/talisman/master/global_install_scripts/install.bash > /tmp/install_talisman.bash && /bin/bash /tmp/install_talisman.bash
+
+
+# pre-commit check sec
+
+# curl --silent  https://raw.githubusercontent.com/thoughtworks/talisman/master/global_install_scripts/install.bash > /tmp/install_talisman.bash && /bin/bash /tmp/install_talisman.bash pre-push
